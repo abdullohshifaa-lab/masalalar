5b28168f165956e4e79e31321b347f0465c8b041
abdullohshifaa-lab <abdullohshifaa@gmail.com>
2026-01-06 11:16:35 +0300

    news

---
+++ masala2.py
@@ -0,0 +1,8 @@
+# 2 - masala
+# set1 = {2, 3, 4, 5, 6}
+# set2 = {4, 5, 6, 7, 8, 9}
+
+# result = set1.symmetric_difference(set2)
+
+# print("umumiy bo’lmagan elementlarning yig'indisi: ",sum(result))
+
---
+++ masala3.py
@@ -0,0 +1,18 @@
+# 3 - masala
+set1={100, 20, 45, 80, 70, 50}
+set2={30, 55, 70, 60, 20, 100}
+
+set3= set1.intersection(set2)
+
+
+for i in set3.copy():
+    if i<60:
+        set3.discard(i)
+
+summa=0
+sanash=0
+for i in set3:
+    summa+=i
+    sanash+=1
+
+print(summa/sanash)
